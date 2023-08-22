6188086ae6e23ceddde2d5d66c7042006855af19
Mike English <mike.english@gmail.com>
2023-08-22 17:12:11 -0400

    WIP: Start wiring up URLProtocol
    Correctly test linking against Rust code

--- libavformat/libffout.c
+++ libavformat/libffout.c
@@ -1,8 +1,17 @@
+#include "libavformat/url.h"
+#include "ffout.h"
+#include <stdio.h>
+
+static int ffout_open1(URLContext *url_ctx, const char *url, int flags){
+    printf("hi from ffout_open1\n");
+    return 0;
+}
+
 const URLProtocol ff_libffout_protocol = {
     .name = "ffout",
     .url_open  = ffout_open,
-    .url_read  = ffout_read,
+//    .url_read  = ffout_read,
     .url_write = ffout_write,
-    .url_close = ffout_close,
-    .priv_data_size = sizeof(MoqTransportContext),
+//    .url_close = ffout_close,
+//    .priv_data_size = sizeof(MoqTransportContext),
 };
